47a25c6b447f2f2c1c5273ad90ccafc5864c7291
s.kudaev <s.kudaev@andersenlab.com>
2025-05-12 21:39:35 +0300

    lesson16 after mobile ready

--- src/test/java/lesson16/android_test/AndroidApplicationTest.java
+++ src/test/java/lesson16/android_test/AndroidApplicationTest.java
@@ -2,19 +2,22 @@ package lesson16.android_test;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import lesson16.android.AndroidPage;
 import lesson16.android.AppiumDriverInit;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class AndroidApplicationTest {
-    //Views //TextClock //android.widget.TextView[4]
+
     AppiumDriver driver;
+    AndroidPage androidPage;
 
 
     @BeforeMethod
     public void setUp(){
         driver = new AppiumDriverInit().getDriver();
+        androidPage = new AndroidPage(driver);
     }
 
     @AfterMethod
@@ -24,7 +27,8 @@ public class AndroidApplicationTest {
 
     @Test
     public void clockTest(){
-        System.out.println("Hello");
-        System.out.println();
+        androidPage.clickOnView();
+        androidPage.scrollUntilTextClockAndClick();
+        androidPage.checkClock();
     }
 }
